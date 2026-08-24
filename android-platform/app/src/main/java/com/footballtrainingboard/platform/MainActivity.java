0cdd6e501da7fc6e1b97d718ef987521524da9c5
razzolinitommaso-prog <razzolinitommaso@gmail.com>
2026-08-24 12:15:36 +0200

    Prevent FTB Platform status bar overlap

--- android-platform/app/src/main/java/com/footballtrainingboard/platform/MainActivity.java
+++ android-platform/app/src/main/java/com/footballtrainingboard/platform/MainActivity.java
@@ -1,5 +1,18 @@
 package com.footballtrainingboard.platform;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        getWindow().setStatusBarColor(Color.rgb(10, 10, 15));
+        getWindow().setNavigationBarColor(Color.rgb(10, 10, 15));
+        super.onCreate(savedInstanceState);
+    }
+}
